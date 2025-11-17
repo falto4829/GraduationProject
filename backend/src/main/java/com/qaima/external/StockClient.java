5cfb1423aab96f67af76a2baa90aef0232be3dfc
falto4829 <dmsrb7161@naver.com>
2025-11-17 16:34:26 +0900

    Update StockClient.java
    add : mono, read multiple ticker

--- backend/src/main/java/com/qaima/external/StockClient.java
+++ backend/src/main/java/com/qaima/external/StockClient.java
@@ -3,10 +3,13 @@ package com.qaima.external;
 import com.qaima.domain.Stock;
 import com.qaima.dto.MarketStackTickersResponse;
 import com.qaima.dto.StockDto;
+import reactor.core.publisher.Mono;
 
 public interface StockClient {
+
     StockDto fetchStock(Stock stock);
 
-    // Marketstack 티커 하나 조회용
-    MarketStackTickersResponse.TickerData fetchTickerMeta(String symbol);
+    Mono<MarketStackTickersResponse.TickerData> fetchTickerMeta(String symbol);
+    Mono<MarketStackTickersResponse> fetchTickers();
+
 }
