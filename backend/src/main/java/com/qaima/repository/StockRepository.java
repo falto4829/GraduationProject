74d2e53a88bc6e41209181d0363ce188f8224b4b
joony0905 <stb1436@naver.com>
2025-11-19 08:26:27 +0900

    Connect Backend api <-> Front

--- backend/src/main/java/com/qaima/repository/StockRepository.java
+++ backend/src/main/java/com/qaima/repository/StockRepository.java
@@ -4,6 +4,7 @@ import com.qaima.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import com.qaima.domain.Exchange;
 
 import java.util.Optional;
 
@@ -15,5 +16,6 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
         where s.stockCode = :stockCode
     """) //TODO: Stock id 조회후 exchange 조인해서 거래상장소 확인 쿼리 예시입니다. db생성하시고 바꿀거있으면 바꿔주세요.
     Optional<Stock> findByStockCodeWithExchange(@Param("stockCode") String stockCode);
+    Optional<Stock> findByExchangeAndStockCode(Exchange exchange, String stockCode);
 }
 
